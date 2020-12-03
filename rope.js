83558deabb7a3db075c5ec8978e4aabceb5a7e6a
ShagunSolanki <72804709+ShagunSolanki@users.noreply.github.com>
2020-12-03 19:34:16 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,48 @@
+class rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.2,
+            length: 400
+        }
+        this.pointB = pointB
+        this.rope = Constraint.create(options);
+        
+        World.add(world, this.rope);
+    }
+
+    fly(){
+        this.rope.bodyA = null;
+    }
+
+    attach(body){
+        this.rope.bodyA = body
+    }
+
+    display(){
+
+         
+          
+        if(this.rope.bodyA){
+
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.pointB;
+
+            push()
+            strokeWeight(10);
+        
+
+        
+
+            stroke("white")
+
+            line(pointB.x, pointB.y, pointA.x, pointA.y);
+           
+             pop()
+         
+        
+        }
+    }
+    
+}
